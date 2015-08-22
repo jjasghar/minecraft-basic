61f4dc27cb2a682664119f933f36a5c723fd685c
JJ Asghar <jj@getchef.com>
2015-08-22 16:21:51 -0500

    added tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,9 +4,28 @@ describe 'minecraft-basic::default' do
 
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe file('/usr/share/minecraft/minecraft_server.jar') do
+    it { should exist }
+  end
+
+  describe file('/usr/share/minecraft/eula.txt') do
+    its(:content) { should match /true/ }
+    it { should exist }
+  end
+
+  describe file('/etc/init/minecraft-server.conf') do
+    its(:content) { should match /minecraft/ }
+    it { should exist }
+  end
+
+  describe process("java") do
+    its(:user) { should eq "root" }
+    it { should be_running }
+  end
+
+  describe port(25565) do
+    it { should be_listening }
   end
 
 end
