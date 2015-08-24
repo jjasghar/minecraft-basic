a1bf2668cdc3ca32336dce1db3cf9e81aba0c54e
JJ Asghar <jj@getchef.com>
2015-08-24 10:16:52 -0500

    Added Changelog and supported platforms.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# CHANGELOG
+
+## 1.0.1 (2015-08-24)
+
+- PR [#6](https://github.com/jjasghar/minecraft-basic/pull/6) by Nathen Harvey
+- Added Supported Platforms
+- Added this CHANGELOG.md
+
+## 1.0.0 (2015-08-24)
+
+- "Production" ready
+- Chefspec added
+- Blog Post referanced
+- CentOS 7 Support
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email 'jj@chef.io'
 license 'Apache v2'
 description 'Installs/Configures minecraft server'
 long_description 'Installs/Configures minecraft server'
-version '1.0.0'
+version '1.0.1'
+
+supports 'ubuntu' , ">= 14.04"
+supports 'centos', ">= 7.0"
 
 source_url 'https://github.com/jjasghar/minecraft-basic'
 issues_url 'https://github.com/jjasghar/minecraft-basic/issues'
